9997cb89f8c56b437b542f2a7b19ba1a913fa56e
SherlockHsu <SherlockHsu@foxmail.com>
2018-03-24 00:07:16 +0800

    更新注释

--- CPP/ecs/predict.cpp
+++ CPP/ecs/predict.cpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 #include <sstream>
-#include <fstream>
 #include <map>
 #include <vector>
 
@@ -16,7 +15,6 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
 {
 
 	std::stringstream ss;						// 字符串流
-	//std::ofstream ss;							// 输出文件流
 	std::string serial, flaName, time, target;	// 虚拟机ID，虚拟机规格，创建时间，优化资源维度名
 	std::string str, strResult;					// 临时str，结果字符串
 	Date date, dateFirst, dateLast;				// 当前日期，data中最早日期，data中最晚日期
@@ -28,7 +26,7 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
 	std::string arrFlaName[MAX_FLAVOR];			// Flavor名数组
 	int arrFlaCPU[MAX_FLAVOR];					// 各Flavor对应CPU核数数组
 	int arrFlaMEM[MAX_FLAVOR];					// 各Flavor对应内存数组
-	int arrFlaPre[MAX_FLAVOR];				// 各Flavor预测结果
+	int arrFlaPre[MAX_FLAVOR];					// 各Flavor预测结果
 	int res[MAX_PHY][MAX_FLAVOR];				// 分配结果数组
 
 	// 读取info
@@ -78,20 +76,7 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
 	}
 	ss.clear();
 
-	//// fstream输出flavor统计表
-	//ofs.open("vecFlavor.txt");
-	//for (int i = 0; i < numFla; i++)
-	//{
-	//	ofs << arrFlaName[i] << ':' << '\t';
-	//	for (int j = 0; j < sumDate; j++)
-	//		ofs << vecData[i][j] << '\t';
-	//	ofs << std::endl;
-	//}
-	//ofs.close();
-
 	// 预测
-	//for (int i = 0; i < numFla; i++)
-	//	arrFlaPre[i] = arrData[i][numPeriod - 1];
 	PredictAll(vecData, period, numFla, arrFlaPre);
 
 	// 计算有效Flavor数
@@ -99,37 +84,13 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
 	for (int i = 0; i < numFla; i++)
 		numFlaValid += arrFlaPre[i];
 
-
 	// 分配
 	for (int i = 0; i < MAX_PHY; i++)
 		for (int j = 0; j < numFla; j++)
 			res[i][j] = 0;
 	numPHY = distribution(sumCPU, sumMEM, numFla, target, arrFlaCPU, arrFlaMEM, arrFlaPre, res);
 
-	// 输出结果
-	//ss << numFlaValid << std::endl;
-	//for (int i = 0; i < numFla; i++)
-	//	ss << arrFlaName[i] << " " << arrFlaPre[i] << std::endl;
-	//ss << std::endl;
-	//ss << numPHY << std::endl;
-	//for (int i = 0; i < numPHY; i++)
-	//{
-	//	ss << i + 1;
-	//	for (int j = 0; j < numFla; j++)
-	//		if (res[i][j] != 0)
-	//			ss << " " << arrFlaName[j] << " " << res[i][j];
-	//	ss << std::endl;
-	//}
-	//getline(ss, str);
-	//getline(ss, strResult);
-	//strResult += '\n';
-	//while (getline(ss, str))
-	//{
-	//	strResult += str;
-	//	strResult += '\n';
-	//}
-	//strResult.pop_back();
-
+	// 生成结果字符串
 	strResult += std::to_string(numFlaValid);
 	strResult += '\n';
 	for (int i = 0; i < numFla; i++)
@@ -156,8 +117,8 @@ void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int da
 		strResult += '\n';
 	}
 
-	// 需要输出的内容
-	char * result_file = (char *)"17\n\n0 8 0 20";
+	//// 需要输出的内容
+	//char * result_file = (char *)"17\n\n0 8 0 20";
 
 	// 直接调用输出文件的方法输出到指定文件中(ps请注意格式的正确性，如果有解，第一行只有一个数据；第二行为空；第三行开始才是具体的数据，数据之间用一个空格分隔开)
 	write_result(strResult.c_str(), filename);
